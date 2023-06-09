1c7746cc7034f8b45457ae614f158f4c0f707537
Arman Kalmakhambetov <arman458792@gmail.com>
2023-06-09 12:10:44 +0600

    git init

---
+++ src/main/java/com/armanK/pet/project/KUS_Web_App/KusWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.armanK.pet.project.KUS_Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/armanK/pet/project/KUS_Web_App/TestKusWebAppApplication.java
@@ -0,0 +1,22 @@
+package com.armanK.pet.project.KUS_Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestKusWebAppApplication {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>("mysql:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(KusWebAppApplication::main).with(TestKusWebAppApplication.class).run(args);
+	}
+
+}
